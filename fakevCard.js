c61d0fbf0d0c511e90d72399225a5f89f395571d
Ahmad x bunty <usamile269@gmail.com>
2026-06-19 20:27:48 +0500

    Add files via upload

---
+++ fakevCard.js
@@ -0,0 +1,15 @@
+module.exports = {
+    fakevCard: {
+        key: {
+            fromMe: false,
+            participant: "0@s.whatsapp.net",
+            remoteJid: "status@broadcast"
+        },
+        message: {
+            contactMessage: {
+                displayName: "© ᴀʜᴍᴀᴅ-ᴍᴅ ᴏꜰꜰɪᴄɪᴀʟ",
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Meta\nORG:META AI;\nTEL;type=CELL;type=VOICE;waid=923234735243:+923234735243\nEND:VCARD`
+            }
+        }
+    }
+};
